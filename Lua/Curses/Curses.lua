930ba9861e44ed3b379b3e3bd0e929001c50c21c
Aitor P. Iturri <aitor.iturri@gmail.com>
2013-10-17 18:21:51 +0200

    Fixed some buges

--- Lua/Curses/Curses.lua
+++ Lua/Curses/Curses.lua
@@ -261,7 +261,7 @@ M.init = function (ncurses, panel, ncursesWrapper)
    end
 end
 
-local getmaxyx = function (win, v)
+local getmaxyx = function (ncursesWrapper, win, v)
    local w
    if not win then
       w = initscr
@@ -269,9 +269,9 @@ local getmaxyx = function (win, v)
       w = win
    end
    if (v == "x") then
-      return ncurses_wrp.getmax_x(w)
+      return ncursesWrapper.getmax_x(w)
    elseif (v == "y") then
-      return ncurses_wrp.getmax_y(w)
+      return ncursesWrapper.getmax_y(w)
    end
 end
 
@@ -289,12 +289,12 @@ M_mt.EndCurses = function (ncurses, panel, ncursesWrapper)
 end
 M_mt.GetMaxX = function (ncurses, panel, ncursesWrapper)
    return function (win)
-      return getmaxyx(w, "x")
+      return getmaxyx(ncursesWrapper, w, "x")
    end
 end
 M_mt.GetMaxY = function (ncurses, panel, ncursesWrapper)
    return function (win)
-      return getmaxyx(w, "y")
+      return getmaxyx(ncursesWrapper, w, "y")
    end
 end
 M_mt.MvPrint = function (ncurses, panel, ncursesWrapper)
@@ -345,129 +345,3 @@ M_mt.WGetCh = function (ncurses, panel, ncursesWrapper)
 end
 
 return Curses
-
---[[local ncurses = alien.load("ncurses")
-local panel = alien.load("panelw")
-local ncurses_wrp = alien.load("C/libcurses_wrp.0.1.so")--]]
-
---local initscr = nil
-
---[[COLOR_BLACK  = 0
-COLOR_RED    = 1
-COLOR_GREEN  = 2
-COLOR_YELLOW = 3
-COLOR_BLUE   = 4
-COLOR_MAGENTA= 5
-COLOR_CYAN   = 6
-COLOR_WHITE  = 7
-
-CURSOR_INVISIBLE   = 0
-CURSOR_NORMAL      = 1
-   CURSOR_VERYVISIBLE = 2--]]
-
-
---}}}
--- Local Functions {{{
---[[local getmaxyx = function (win, v)
-	local w
-	if not win then
-		w = initscr
-	else
-		w = win
-	end
-	if (v == "x") then
-		return ncurses_wrp.getmax_x(w)
-	elseif (v == "y") then
-		return ncurses_wrp.getmax_y(w)
-	end
-end
--- }}}
--- Exported Functions {{{
-InitCurses = function ()
-	initscr = ncurses.initscr()
-	return initscr
-end
-
-EndCurses = function ()
-	init = false
-	return ncurses.endwin()
-end
-
-GetMaxX = function (win)
-	return getmaxyx(w, "x")
-end
-
-GetMaxY = function (win)
-	return getmaxyx(w, "y")
-end
-
-MvPrint = function (w, y, x, str)
-	if w then
-		return ncurses.mvwprintw(w, y, x, str)
-	else
-		return ncurses.mvwprintw(initscr, y, x, str)
-	end
-end
-
-Print = function (w, str)
-	if w then
-		return ncurses.wprintw(w, str)
-	else
-		return ncurses.wprintw(initscr, str)
-	end
-end
-
-Refresh = function ()
-	return ncurses.refresh()
-end
-
-GetCh = function ()
-	local c = ncurses.getch()
-	if c <= 31 or c >= 256 and c <= 511 then
-		return Curses.Keys[c] or "KEY_UNKNOW"
-	end
-	return string.char(c)
-end
-WGetCh = function (win)
-	local c = ncurses.wgetch(win)
-
-	if c < 0 then
-		return "KEY_UNKNOW"
-	end
-
-	if c <= 31 or c >= 256 and c <= 511 then
-		return Curses.Keys[c] or "KEY_UNKNOW"
-	end
-
---	return string.char(ncurses.getch())
-	return string.char(c)
-   end--]]
-
---GetCh = ncurses.getch
---[[NewWin = ncurses_wrp._new_win
-DelWin = ncurses.delwin
-Box = ncurses_wrp._box
-ColorPair = ncurses_wrp._color_pair
-WRefresh = ncurses.wrefresh
-WBorder = ncurses_wrp._wborder
-HasColors = ncurses.has_colors
-StartColor = ncurses.start_color
-InitPair = ncurses.init_pair
-AttrOn = ncurses.attron
-WAttrOn = ncurses.wattron
-NoEcho = ncurses.noecho
-Echo = ncurses.echo
-CBreak = ncurses.cbreak
-UseDefaultColors = ncurses.use_default_colors
-CursSet = ncurses.curs_set
-KeyPad = ncurses.keypad
-WErase = ncurses.werase
-WClear = ncurses.wclear
-WClrToEol = ncurses.wclrtoeol
-WMove = ncurses.wmove
-HalfDelay = ncurses.halfdelay
-Timeout = ncurses.timeout
-NewPanel = panel.new_panel
-   UpdatePanels = panel.update_panels--  ]]
--- }}}
----- vi: set foldmethod=marker foldlevel=0:
